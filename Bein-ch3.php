1933530ea8b7aa5fae75ced0ebc6313f146a96cb
sayedkhaled <sayedkhaledsayed@gmail.com>
2016-10-18 17:43:21 +0200

    Add files via upload

---
+++ Bein-ch3.php
@@ -0,0 +1,28 @@
+<html>
+<head>
+<title>Kora 4U | Bein 3</title>
+</head>
+<body>
+<style>*{margin:0;padding:0;}</style>
+<script data-cfasync="false" src="https://rawgit.com/sayedkhaled/site/master/player.js"></script>
+<div id="playerjxJRqLMFuJaA">
+</div>
+<script data-cfasync="false" type="text/javascript">
+    jwplayer('playerjxJRqLMFuJaA').setup({
+    file: 'rtmp:\/\/edgetee-frt3-1.xx.fbcdn.net:80\/live-md\/655480391286059?api=1&b=2000&o=lla2&a=AfAhskX5FA-E6Rrd',
+  aboutlink: 'http://www.sayedkhaled.blogspot.com',
+        controls: 'true',
+        title: 'Live Stream',
+        width: '100%',
+        height: '490',
+        stretching: 'uniform',
+        autostart: 'true',
+        primary: 'flash',
+ skin: 'glow',
+ androidhls: 'true',
+image: "https://2.bp.blogspot.com/-JM3VsHCLFog/WAY2Q-bO2LI/AAAAAAAABlk/zySNhH0XsZY_o7SBr8mujqn1CldyjCGpACLcB/s1600/kora4u.png",
+
+    });
+</script>
+</body>
+</html>
